bd74dc21e987006df85b9922d56216cd3bf3b5e4
Vishwajeet Kore <123283352+vishwajeetkore@users.noreply.github.com>
2026-05-13 00:27:36 +0530

    Update backend.tf

--- pharma-devops/terraform/envs/dev/backend.tf
+++ pharma-devops/terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pharma-tf-state-873135413040"
+    bucket         = "pharma-tf-state-531262218012"
     key            = "envs/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
